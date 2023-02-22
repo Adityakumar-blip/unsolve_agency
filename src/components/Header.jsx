e083f03d9ee6554cb532a725ea2c3f889148c1ed
adityakumar-blip <adityakamal303@gmail.com>
2023-02-22 23:32:35 +0530

    hamburger menu added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,25 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import { Button, IconButton } from '@mui/material';
-import HeroPage from './Home/HeroPage';
-import { GiHamburgerMenu } from 'react-icons/gi'
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { Button, IconButton } from "@mui/material";
+import HeroPage from "./Home/HeroPage";
+import { GiCrossMark, GiHamburgerMenu } from "react-icons/gi";
+import { RxCross2 } from 'react-icons/rx'
 
-const navItems = ['Home', 'About', 'Contact'];
+const navItems = ["Home", "About", "Contact", "Works", 'Sales'];
 
 function ElevationScroll(props) {
   const { children, window } = props;
   // Note that you normally won't need to set the window ref as useScrollTrigger
   // will default to window.
   // This is only being set here because the demo is in an iframe.
+
   const trigger = useScrollTrigger({
     disableHysteresis: true,
     threshold: 0,
@@ -39,43 +41,67 @@ ElevationScroll.propTypes = {
 };
 
 export default function ElevateAppBar(props) {
+  const [open, setOpen] = React.useState(false);
+
+  const hamburgerMenu = () => {
+    setOpen(true);
+  };
+
+  const handleClick = () => {
+    setOpen(false)
+  }
+
+  console.log(open);
   return (
     <React.Fragment>
       <CssBaseline />
       <ElevationScroll {...props}>
-      <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar component="nav">
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            
-            sx={{ mr: 2, display: { sm: 'none' } }}
-          >
-            <GiHamburgerMenu/>
-          </IconButton>
-          <Typography
-            variant="h5"
-            component="div"
-            sx={{ flexGrow: 1,}}
-          >
-            Unsolved Agency
-          </Typography>
-          {/* <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+        {open == true ? (
+          <div className="main-nav">
+          <div className="nav-container">
+            <div className="closeIcon">
+              <RxCross2 size={35} color="white" onClick={handleClick} />
+            </div>
+            <div className="nav-items">
             {navItems.map((item) => (
-              <Button key={item} sx={{ color: '#fff' }}>
-                {item}
+              <Button key={item} sx={{ color: "#fff" }}>
+                <Typography fontSize='2rem' fontWeight='600' color='white' >{item}</Typography>
               </Button>
             ))}
-          </Box> */}
-        </Toolbar>
-      </AppBar>
-    </Box>
+            </div>
+          </div>
+          </div>
+        ) : (
+          <Box sx={{ display: "flex" }}>
+            <CssBaseline />
+            <AppBar component="nav">
+              <Toolbar>
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  edge="start"
+                  onClick={() => hamburgerMenu()}
+                  sx={{ mr: 2, display: { sm: "none" } }}
+                >
+                  <GiHamburgerMenu />
+                </IconButton>
+                <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
+                  Unsolved Agency
+                </Typography>
+                <Box sx={{ display: { xs: "none", sm: "block" } }}>
+                  {navItems.map((item) => (
+                    <Button key={item} sx={{ color: "#fff" }}>
+                      {item}
+                    </Button>
+                  ))}
+                </Box>
+              </Toolbar>
+            </AppBar>
+          </Box>
+        )}
       </ElevationScroll>
       <Toolbar />
-    <HeroPage/>
+      <HeroPage />
     </React.Fragment>
   );
 }
